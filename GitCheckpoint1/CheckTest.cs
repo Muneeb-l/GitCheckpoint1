675a4b6c3457a3f322427587ecc041a196f45c18
munee <muneeb.irk@gmail.com>
2023-02-16 14:09:22 +0100

    added new class and method

---
+++ GitCheckpoint1/CheckTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckpoint1
+{
+    public class CheckTest
+    {
+        public static void TestMessage(string n)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
